bd670ff4c9aa1be4d9454c58c549ce47865eaf01
root <root@tony.>
2025-04-18 21:18:10 -0400

    Your commit message

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-
+function calculateTax(amount) {
+    const tax = amount * 0.10;
+    return tax;
+  }
+  
+  function convertToUpperCase(text) {
+    return text.toUpperCase();
+  }
+  
+  function findMaximum(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+  }
+  
+  function isPalindrome(word) {
+    const reversed = word.split('').reverse().join('');
+    return word === reversed;
+  }
+  
+  function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const discountAmount = (originalPrice * discountPercentage) / 100;
+    return originalPrice - discountAmount;
+  }
+  
+  
 
 
 
